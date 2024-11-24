0f941490230bc6dad2006832ec43050ba20579b5
Mandala <xiconz@qq.com>
2024-11-24 12:50:43 +0800

    build: release version

---
+++ internal/biz/greeter.go
@@ -0,0 +1,28 @@
+package biz
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+// GreeterRepo is a Greater repo.
+type GreeterRepo interface {
+}
+
+// GreeterUsecase is a Greeter usecase.
+type GreeterUsecase struct {
+	repo GreeterRepo
+	log  *log.Helper
+}
+
+// NewGreeterUsecase new a Greeter usecase.
+func NewGreeterUsecase(repo GreeterRepo, logger log.Logger) *GreeterUsecase {
+	return &GreeterUsecase{repo: repo, log: log.NewHelper(logger)}
+}
+
+// CreateGreeter creates a Greeter, and returns the new Greeter.
+func (uc *GreeterUsecase) CreateGreeter(ctx context.Context, g *Greeter) (*Greeter, error) {
+	uc.log.WithContext(ctx).Infof("CreateGreeter: %v", g.Hello)
+	return uc.repo.Save(ctx, g)
+}
---
+++ internal/data/greeter.go
@@ -0,0 +1,20 @@
+package data
+
+import (
+	"payment/internal/biz"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type greeterRepo struct {
+	data *Data
+	log  *log.Helper
+}
+
+// NewGreeterRepo .
+func NewGreeterRepo(data *Data, logger log.Logger) biz.GreeterRepo {
+	return &greeterRepo{
+		data: data,
+		log:  log.NewHelper(logger),
+	}
+}
